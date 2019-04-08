972423d53b1d77aadbde7b48918580d75e936225
Juozas Rastenis <juozas.rastenis@gmail.com>
2019-04-08 20:20:38 +0200

    More useEffect hooks added to interact with state

--- src/routes/GamePage.tsx
+++ src/routes/GamePage.tsx
@@ -3,39 +3,66 @@ import {selfpong} from '../gamesLogic/selfpong';
 import { RouteComponentProps } from 'react-router-dom';
 import firebase from '../firebase';
 import {Game} from '../models/Game.model';
+import DynamicButtoon from '../components/DynamicButton';
 
+let gameCanvas;
 const GamePage: FunctionComponent<any> = (props: RouteComponentProps) => {
   const [gameInfo, setGameInfo] = useState<Game>({name: '', complexity: 0});
-  const gameId = props.match.params.id;
-  let gameCanvas;
-  useEffect(() => {
-    waitForInformation().then(() => {
-      switch(gameInfo.name) {
-        case 'Self pong':
-          console.log('yo');
-          break;
-        case 'Other':
-          break;
-        default:
-          // code block
-      }
-    })
-  }, []);
+  const [gameLost, setGameLost] = useState<Boolean>(false);
+  const [score, setScore] = useState<Number>(0);
 
-  async function waitForInformation() {
-    if (!!props.location.state && !!props.location.state.game) {
-      await setGameInfo(props.location.state.game);
-    } else {
-      await firebase.getOneById('games', gameId).then(data => {
-        setGameInfo(data.data());
-      })
+  useEffect(() => {
+    const gameId = props.match.params.id;
+    firebase.getOneById('games', gameId).then(data => {
+      setGameInfo(data.data());
+    })
+  }, [])
+
+  useEffect(() => {
+    console.log('received: ' + gameInfo.name);
+    switch(gameInfo.name) {
+      case 'Self pong':
+        gameCanvas = selfpong(score => {
+          setScore(score);
+          setGameLost(true);
+        })
+        break;
+      case 'Other':
+        break;
+      default:
+        // code block
     }
-  }
+  }, [gameInfo]);
+
+  useEffect(() => {
+    if (gameLost) gameCanvas.remove();
+  }, [gameLost])
+  
   return (
-    <div className="content-section">
+    <div className="content-section content-center">
       <h1 className="title">{gameInfo ? gameInfo.name : 'Loading'}</h1>
-      <div id="myContainer"></div>
+      {gameLost ? 
+      <div className="content-center">
+        <h1>{score}</h1>
+        <DynamicButtoon name="helloButton" info="someinfo"></DynamicButtoon>
+        <button onClick={() => { 
+          setGameLost(false);
+          setGameInfo({name: gameInfo.name, complexity: gameInfo.complexity});
+        }}>Restart Game</button> 
+      </div>
+      : 
+      <div id="myContainer"></div>}
     </div>
   );
 }
 export default GamePage
+
+// async function waitForInformation() {
+  //   if (!!props.location.state && !!props.location.state.game) {
+  //     await setGameInfo(props.location.state.game);
+  //   } else {
+  //     await firebase.getOneById('games', gameId).then(data => {
+  //       setGameInfo(data.data());
+  //     })
+  //   }
+  // }
