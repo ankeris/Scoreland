44c8daff821717a3b0e87db9ca3e545d4c9b9384
Juozas Rastenis <juozas.rastenis@gmail.com>
2019-05-12 14:51:08 +0200

    Favicon, Manifest.json added

--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -1,32 +1,37 @@
-import React, { FunctionComponent, useState, useEffect } from 'react';
-import { RouteComponentProps } from 'react-router-dom';
-import firebase from '../firebase';
+import React, { FunctionComponent, useState, useEffect } from "react";
+import { RouteComponentProps, Link } from "react-router-dom";
+import firebase from "../firebase";
 
-const Header: FunctionComponent<RouteComponentProps> = (props) => {
-  return (
-    <header className="App-header bg-dark">
-        <div className="flex-row">
-            <img className="App-logo" src="/logo.svg" alt=""/>
-            <h1>Scoreland</h1>
-        </div>
-        <div className="flex-row">
-        {props.authed ? 
-            <>
-            <div onClick={logout} className="push-horizontal button button--small button--serious button--danger">Logout</div>
-            <div className="push-horizontal">Hi, {localStorage.getItem('user')}</div>
-            </>
-            :
-            <div className="push-horizontal">Not logged in</div>
-        }
-        </div>
-    </header>
-  );
-  async function logout() {
-    console.log('trying to log out');
-    await firebase.logout().then(() => {
-      props.setAuth(false);
-    });
-  }
-}
+const Header: FunctionComponent<RouteComponentProps> = props => {
+    return (
+        <header className="App-header bg-dark">
+            <div className="flex-row">
+                <img className="App-logo" src="/logo.svg" alt="" />
+                <h1>Scoreland</h1>
+                <Link to="/highscores" className="push-horizontal button button--small">
+                    Highscores
+                </Link>
+            </div>
+            <div className="flex-row">
+                {props.authed ? (
+                    <>
+                        <div onClick={logout} className="push-horizontal button button--small button--serious button--danger">
+                            Logout
+                        </div>
+                        <div className="push-horizontal">Hi, {localStorage.getItem("user")}</div>
+                    </>
+                ) : (
+                    <div className="push-horizontal">Not logged in</div>
+                )}
+            </div>
+        </header>
+    );
+    async function logout() {
+        console.log("trying to log out");
+        await firebase.logout().then(() => {
+            props.setAuth(false);
+        });
+    }
+};
 
-export default Header
+export default Header;
