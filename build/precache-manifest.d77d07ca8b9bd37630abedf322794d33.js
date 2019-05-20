ac381d1433e12fb458549ce854b5ec7c12a97a16
Juozas Rastenis <juozas.rastenis@gmail.com>
2019-05-21 00:05:40 +0200

    build folder added to git in order to deploy

---
+++ build/precache-manifest.d77d07ca8b9bd37630abedf322794d33.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "189784455225dfef94ae",
+    "url": "/static/js/main.16a89bc3.chunk.js"
+  },
+  {
+    "revision": "111a435dc23c08334ed7",
+    "url": "/static/js/2.8c86c6de.chunk.js"
+  },
+  {
+    "revision": "111a435dc23c08334ed7",
+    "url": "/static/css/2.2ce6c78d.chunk.css"
+  },
+  {
+    "revision": "26afc82c8157029aef3efb3c353e8e7a",
+    "url": "/index.html"
+  }
+];
---
+++ build/sw.js
@@ -0,0 +1,77 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
+if (workbox) {
+    console.log('workbox is working');
+
+    self.addEventListener('install', (event) => {
+      self.skipWaiting();
+    });
+    
+    self.addEventListener('activate', event => {
+      event.waitUntil(clients.claim());
+    });
+
+    /* Set debug to true, for console logging */
+    workbox.setConfig({
+      debug: false
+    });
+    
+    workbox.routing.registerNavigationRoute("/index.html", {
+      blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+    });
+    
+    /* Cache doc (HTML) */
+    // workbox.routing.registerRoute(
+    //   new RegExp(/(\/)((\?utm.*)$|$)/),
+    //   new workbox.strategies.CacheFirst({
+    //       cacheName: "doc"
+    //   })
+    // ); 
+
+    // Caching Content from Multiple Origins
+    workbox.routing.registerRoute(
+      /.*(?:googleapis|gstatic|firestore)\.com.*$/,
+      new workbox.strategies.NetworkFirst({
+          cacheName: "google-apis",
+          plugins: [
+              new workbox.cacheableResponse.Plugin({
+                  statuses: [0, 200]
+              })
+          ]
+      })
+    ); 
+
+    /* Cache CSS and JavaScript Files */
+    workbox.routing.registerRoute(
+      new RegExp(/(css|js|json|webmanifest)((\?.*)$|$)/),
+      new workbox.strategies.CacheFirst({
+          cacheName: "static-resources"
+      })
+    );
+    
+    /* Cache Font Files */
+    workbox.routing.registerRoute(
+      new RegExp(/(woff|woff2|ttf|otf)((\?.*)$|$)/),
+      new workbox.strategies.CacheFirst({
+          cacheName: "static-fonts"
+      })
+    ); 
+
+    // Other types of files that are most likely not going to change
+
+    /* Caching Images */
+    workbox.routing.registerRoute(
+      new RegExp(/(jpg|jpeg|gif|png|svg|ico)((\?.*)$|$)/),
+      new workbox.strategies.CacheFirst({
+          cacheName: "images",
+          plugins: [
+              new workbox.expiration.Plugin({
+                  maxEntries: 60,
+                  maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+              })
+          ]
+      })
+    );
+} else {
+    console.log(`Boo! Workbox didn't load 😬`);
+}
