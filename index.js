1aaf556b2198e09b685833de82a700132119137b
Juozas Rastenis <juozas.rastenis@gmail.com>
2019-05-21 00:13:36 +0200

    port changed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 9000;
+const port = 80;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
