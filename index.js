29e3190dfbf028518e50a60ff574311bb3dc97bf
Juozas Rastenis <juozas.rastenis@gmail.com>
2019-05-21 00:23:56 +0200

    test

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port, () => console.log(`Example app: http://localhost:${port}`))
+app.listen(process.env.PORT || port, () => console.log(`Example app: http://localhost:${port}`))
